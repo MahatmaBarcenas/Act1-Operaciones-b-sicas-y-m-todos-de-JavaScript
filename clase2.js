5910608f7de6c780047296dfc5089de6d99fb76e
MahatmaBarcenas <booster1.2016@gmail.com>
2022-12-02 17:18:43 -0800

    Update

---
+++ clase2.js
@@ -0,0 +1,9 @@
+var nombre = "Dayana", apellido = "Perez";
+
+var NombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraletradelnombre = nombre.charAt(0);
+var cantidaddeletrasdelnombre = nombre.length;
+
+var nombrecompleto = `${nombre} ${apellido.toUpperCase()}`;
---
+++ clase3.js
@@ -0,0 +1,21 @@
+var a = 5;
+var b = 10;
+
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+
+//Esto es un comentario
+
+var precio = 200.3;
+
+var total = precio * 100 * 3 / 100;
+
+var totalStr = total.toFixed(2);
+var total2 = parseFloat(totalStr);
+
+//Mostrar con 2 decimales con variable numerica
+
+console.log("Resultado")
+console.log(total.toFixed(2));
